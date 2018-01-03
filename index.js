79e228d6a006fcb9dec30c71d2b3c509457cff6d
Marta Patron <marta.patron18@gmail.com>
2018-01-03 18:20:29 +0200

    add socket.io

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let app = require('express')();
 let http = require('http').Server(app);
+let io = require('socket')(http);
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname, '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+  });
 
 http.listen(8000, function() {
   console.log('listening on *:8000');
