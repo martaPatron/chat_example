2884a94aac283e64adf8a1aba0f757a4419ca33d
Marta Patron <marta.patron18@gmail.com>
2018-01-04 15:23:03 +0200

    change to two pages

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-let app = require('express')();
+let express = require('express');
+let app = express();
+app.use(express.static(__dirname));
+
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile('index.html');
 });
 
 io.on('connection', function(socket) {
-    console.log('a user connected');
-    socket.on('disconnect', function() {
-        console.log('user disconnect');
-    });
+    // console.log('a user connected');
+    // socket.on('disconnect', function() {
+    //     console.log('user disconnect');
+    // });
     socket.on('chat message', function(msg) {
       io.emit('chat message', msg);
     });
---
+++ showSocketMessage.js
@@ -0,0 +1,12 @@
+$(function() {
+    let socket = io();
+    $('form').submit(function() {
+      socket.emit('chat message', $('#message').val());
+      $('#message').val('');
+      return false;
+    });
+    socket.on('chat message', function(msg) {
+        $('#messages').append($('<li>').text(msg));
+      });
+  });
+
