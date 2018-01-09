1e1703d37fa20fe4a0e49b8b8f50e326be61d2fb
Marta Patron <marta.patron18@gmail.com>
2018-01-09 19:00:36 +0200

    login user with socket

--- index.js
+++ index.js
@@ -9,11 +9,19 @@ app.get('/', function(req, res) {
   res.sendFile('index.html');
 });
 
+
 io.on('connection', function(socket) {
     socket.on('chat message', function(msg) {
       io.emit('chat message', msg);
     });
-    socket.emit('login user');
+
+    socket.emit('login user', function(data) {
+       socket.broadcast.emit('login user', {
+         username: data
+       });
+    });
+
+    socket.broadcast.emit('disconnect');
 });
 
 http.listen(8000, function() {
--- showSocketMessage.js
+++ showSocketMessage.js
@@ -1,9 +1,9 @@
 $(function() {
-    // console.log(localStorage);
+    console.log(localStorage);
     let currentName = localStorage.getItem('currentUser');
     $('.name').text(currentName);
     let socket = io();
-    socket.nameUser = currentName;
+
     $('form').submit(function() {
       socket.emit('chat message', $('#message').val());
       $('#message').val('');
@@ -12,7 +12,7 @@ $(function() {
 
     socket.on('chat message', function(msg) {
       let date = new Date;
-      let message = `${currentName}: \n ${msg}`;
+      let message = `${currentName}: ${msg}`;
       let timeMessage = `${date.getHours()}:${date.getMinutes()}`;
         $('#messages').append($('<li>').text(message).append($('<span>').text(timeMessage)));
       });
@@ -32,19 +32,30 @@ $(function() {
       let newName = $('.another-name').val();
       let name = localStorage.getItem(newName);
       if (newName && !name) {
+        let oldName = localStorage.getItem('currentUser');
+        localStorage.removeItem(oldName);
         localStorage.setItem('currentUser', newName);
         currentName = newName;
         $('.name').text(newName);
         $('.another-name').val('');
-        $('.new-name-box').toggle('slow');
-      } else {
-        alert('Already exist');
       }
+      $('.new-name-box').toggle('slow');
     });
 
-    socket.on('login user', function() {
-      console.log(`userName: ${socket.nameUser}`);
-      $('#online-users').append($('<li>').text(currentName));
+    socket.on('login user', function(data) {
+      console.log(currentName);
+      addNewUser(currentName);
+      // show online users from redis
     });
+
+
+    socket.on('disconnect', function() {
+      console.log('user logout');
+      let msg = `${currentName} logout`;
+      $('#online-users').append($('<li>').text(msg));
+    });
+
+    function addNewUser(user) {
+      $('#online-users').append($('<li>').text(user));
+    }
   });
-
