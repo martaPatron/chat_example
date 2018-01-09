98489584b9bbd52122bc921a00e851689899220d
Marta Patron <marta.patron18@gmail.com>
2018-01-09 23:07:05 +0200

    some changes

--- index.js
+++ index.js
@@ -1,21 +1,68 @@
 let express = require('express');
 let app = express();
-app.use(express.static(__dirname));
-
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+let port = 8000;
 
+app.use(express.static(__dirname));
 app.get('/', function(req, res) {
-  res.sendFile('index.html');
+    res.sendFile('index.html');
 });
+
+let socket = io.connect('http://localhost:' + port);
 
 io.on('connection', function(socket) {
     socket.on('chat message', function(msg) {
-      io.emit('chat message', msg);
+        io.emit('chat message', msg);
     });
-    socket.emit('login user');
+    // socket.emit('login user');
+
+    socket.broadcast.emit('newUser');
+    socket.emit('userName');
+
+    var addedUser = false;
+
+    // when the client emits 'new message', this listens and executes
+    socket.on('new message', function(data) {
+        // we tell the client to execute 'new message'
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
+    });
+
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', function(username) {
+        if (addedUser) return;
+
+        // we store the username in the socket session for this client
+        socket.username = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function() {
+        if (addedUser) {
+            --numUsers;
+
+            // echo globally that this client has left
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    });
 });
 
-http.listen(8000, function() {
-  console.log('listening on *:8000');
+http.listen(port, function() {
+    console.log('listening on *:8000');
 });
